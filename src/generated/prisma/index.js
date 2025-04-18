eba28d8dad8e2e1ef431531459e52e44646a1dc9
DRE_1314 <vivek1314gurjar@gmail.com>
2025-04-19 01:30:40 +0530

    prisma connection setup

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -157,11 +157,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:vynkvivek@db.xvcoxtzuadgktmweszom.supabase.co:5432/postgres"
+        "value": null
       }
     }
   },
