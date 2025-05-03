8a15e7e10f85a833b7fcc37afde0f9f6624f3cfe
DRE_1314 <vivek1314gurjar@gmail.com>
2025-05-03 22:08:54 +0530

    major ui changes of landing-page

--- src/app/landingpage/page.tsx
+++ src/app/landingpage/page.tsx
@@ -1,18 +1,32 @@
 'use client';
 
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { motion } from "framer-motion";
 import { auth } from '@/lib/firebase';
 import { onAuthStateChanged, signInWithPopup ,browserLocalPersistence, setPersistence, GoogleAuthProvider} from 'firebase/auth';
+import Navbar from '@/component/navbar';
 
 
 export default function LandingPage() {
 
-  const [src, setSrc] = useState('/video_assests/bkg.mp4');
-  const [loop, setLoop] = useState(false);
 
   const router = useRouter();
+    const [checking, setChecking] = useState(true);
+  
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, async (user) => {
+        if (user) {
+          router.push('/');
+          setTimeout(() => {
+            setChecking(false);
+          }, 2000);
+        } else {
+          setChecking(false);
+        }
+      });
+  
+      return () => unsubscribe();
+    }, [router]);
 
   const handleGoogleSignIn = async () => {
     try {
@@ -25,56 +39,181 @@ export default function LandingPage() {
     }
   };
 
-
+  if (checking) {
+    return <div className="flex h-screen w-full justify-center items-center">LOADING.....😊🙏</div>; 
+  }
+  
     return(
-      <main className="w-full h-[100vh] flex flex-col items-start justify-between overflow-hidden relative">
+      <main className='relative  w-full bg-[#fefffe] pt-2 pb-2 overflow-hidden'>
+          <Navbar/>
+
+          {/* main heading text */}
+          <div className="w-full py-4 mt-30 flex flex-col ">
+            <h3 className='funnel-regular sm:text-[8vh] text-[1.8rem] flex justify-center items-center text-[#010100]'>No bios. No noise.</h3>
+            <h4 className='funnel-regular sm:text-[8vh] text-[1.8rem] flex justify-center items-center text-[#6a6b6b]'>Just you, your mind, and</h4>
+            <h4 className='funnel-regular sm:text-[8vh] text-[1.8rem] flex justify-center items-center text-[#010100]'>someone who gets it."</h4>
+          </div>
+
+          {/* video */}
+          <div className="relative w-full h-[50vh] sm:h-[80vh] overflow-hidden">
           <video
-        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
-        src={src}
-        autoPlay
-        muted
-        playsInline
-        loop={loop}
-        preload="auto"
-        onEnded={() => {
-          if (!loop) {
-            setSrc('/video_assests/bkg2loop.mp4');
-            setLoop(true);
-          }
-        }}
-      />
+              className="w-full h-full scale-280 sm:scale-120 transform  sm:-translate-y-[5rem]"
+              src="video_assests/mainvid.mp4"
+              autoPlay
+              muted
+              loop
+              playsInline
+            ></video>
+            <button onClick={() => handleGoogleSignIn()} className='absolute funnel-regular text-[1.2rem] sm:text-[1.5vw] backdrop-blur-md bg-white/10 border hover:bg-white/70  border-white/20 rounded-full px-3 py-3 top-1/2 left-1/2 transform -translate-x-1/2  sm:-translate-y-[7rem] -translate-y-[2rem] text-black focus:outline-none'>Unlock</button>
+          </div>
 
-      <div className="w-full sm:mt-26 mt-50 px-4 flex flex-col items-start justify-center">
-        {
-          ["SHARE " ,"THOUGHTS" , "NOT" , "RESUMES"].map((text , index) => {
-            return (
-              <div key={index}  className='overflow-hidden'>
-              <motion.div
-              initial={index!==1 && { y: 80, opacity: 1 }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{
-                duration: 0.4,
-                delay: 5.5,   
-                ease: "easeOut"
-              }}
-              className={`sm:text-[5.8rem] text-[13vw] tracking-tighter sm:leading-20 leading-[12vw] font3 text-center `}>
-                {text}
-              </motion.div>
+          {/* how it works */}
+          <div className=" sm:h-[45vw] h-[150vh] w-full flex px-4 flex-col gap-6 pb-10">
+            <h3 className='text-black funnel-regular text-[2rem] sm:text-[6vh] '>How It Works</h3>
+            <section className='flex flex-col sm:flex-row justify-between items-center w-full h-full gap-2'>
+              <div className="h-full w-full sm:w-1/3 rounded-lg bg-[url('https://i.pinimg.com/736x/d1/a6/95/d1a695e235b5fa08d657dd6253eb45d8.jpg')] bg-cover object-cover bg-opacity-50  p-4">
+                <h4 className='text-black funnel-light text-[2.5vh]'>Type What's On Your Mind </h4>
+                <span className='text-[#6a6b6b] funnel-light text-[0.9rem] sm:text-[1.2vw] leading-none'>Just a thought. A feeling. A moment that’s real, right — now.</span>
               </div>
-            )
-          })
-        }
-      </div>
+              <div className="h-full w-full sm:w-1/3 rounded-lg bg-[url('https://i.pinimg.com/736x/3f/d1/d0/3fd1d0e36fac91096427175bf89dd9f2.jpg')] bg-cover object-cover  p-4">
+                <h4 className='text-black funnel-light text-[2.5vh]'>Get Matched Instantly</h4>
+                <span className='text-[#6a6b6b] funnel-light text-[0.9rem] sm:text-[1.2vw] leading-none'>Vynk finds someone whose mind aligns with yours — no bios, just thoughts.</span>
+              </div>
+              <div className="h-full w-full sm:w-1/3 rounded-lg bg-[url('https://i.pinimg.com/736x/57/01/88/57018805b1c89f10b90f271fe1c5a1ca.jpg')] bg-cover objrct-cover  p-4">
+                <h4 className='text-black funnel-light text-[2.5vh]'>Chat in Real Time</h4>
+                <span className='text-[#6a6b6b] funnel-light text-[0.9rem] sm:text-[1.2vw] leading-none'>Talk anonymously. Think together. Or move on — your space, your pace.</span>
+              </div>
+            </section>
+          </div>
 
-      <hr className=" my-9 border-t border-gray-300 opacity-50 w-full" />
+          {/* features */}
+          <div className="w-full px-4 mt-6">
+            <h2 className='text-black text-[6vh] mb-4 funnel-regular'>Features</h2>
+          <div className="grid grid-cols-2 grid-rows-6 sm:grid-cols-6 sm:grid-rows-2 gap-4 h-full w-full ">
+              <div className='bg-[#eeefef] rounded-lg flex flex-col gap-1 justify-center items-center funnel-regular text-[#010100] py-20'>
+                <span>Matchmaking</span>
+                <img className='w-6 h-6' src="images_assests/icon1.png" alt="" />
+              </div>
+              <div className='bg-[#eeefef] rounded-lg flex flex-col gap-1 justify-center items-center funnel-regular text-[#535353]'>
+              <span>Thought Sharing</span>
+              <img className='w-6 h-6' src="images_assests/icon2.png" alt="" />
+              </div>
+              <div className='bg-[#eeefef] rounded-lg flex flex-col justify-center items-center funnel-regular text-[#010100]'>
+              <span>Real-Time Chat</span>
+              <img className='w-6 h-6' src="images_assests/icon3.png" alt="" />
+              </div>
+              <div className='bg-[#eeefef] rounded-lg flex flex-col justify-center items-center funnel-regular text-[#535353]'>
+              <span>Mood-Based Aliases</span>
+              <img className='w-6 h-6' src="images_assests/icon4.png" alt="" />
+              </div>
+              <div className='bg-[#eeefef] rounded-lg flex flex-col justify-center items-center funnel-regular text-[#010100]'>
+              <span>Anonymity Layers</span>
+              <img className='w-6 h-6' src="images_assests/icon5png.png" alt="" />
+              </div>
+              <div className='bg-[#eeefef] rounded-lg flex flex-col justify-center items-center funnel-regular text-[#535353]'>
+              <span>Smart Input</span>
+              <img className='w-6 h-6' src="images_assests/icon6.png" alt="" />
+              </div>
+              <div className=" bg-[#eeefef] rounded-lg flex flex-col justify-center items-center funnel-regular text-[#010100]">
+              <img className='w-6 h-6' src="images_assests/icon12.png" alt="" />
+              <span>AI Matching</span>
+              </div>
+              <div className="bg-[#eeefef] rounded-lg flex flex-col justify-center items-center funnel-regular text-[#535353]">
+              <img className='w-6 h-6' src="images_assests/icon11.png" alt="" />
+              <span>History Log</span>
+              </div>
+              <div className=" bg-[#eeefef] rounded-lg flex flex-col justify-center items-center funnel-regular text-[#010100]">
+              <img className='w-6 h-6' src="images_assests/icon10.png" alt="" />
+              <span>Context Control</span>
+              </div>
+              <div className=" bg-[#eeefef] rounded-lg flex flex-col justify-center items-center funnel-regular text-[#535353]">
+              <img className='w-6 h-6' src="images_assests/icon9.png" alt="" />
+              <span>Wait Experience</span>
+              </div>
+              <div className=" bg-[#eeefef] rounded-lg flex flex-col justify-center items-center funnel-regular text-[#010100]">
+              <img className='w-6 h-6' src="images_assests/icon8.png" alt="" />
+              <span>Session Replay</span>
+              </div>
+              <div className=" bg-[#eeefef] rounded-lg flex flex-col justify-center items-center funnel-regular text-[#535353]">
+              <img className='w-6 h-6' src="images_assests/icon5png.png" alt=""  />
+              <span>Anonymity</span>
+              </div>
+          </div>
+          </div>
 
-      <div onClick={handleGoogleSignIn}  className="group place-i w-80 h-20 bg-[#232a5a] ml-5 mb-2 rounded-lg flex justify-between items-end p-2">
-        <span className='uppercase font4 text-sm'>get started</span>
-        <section className='bg-white p-[0.2rem] rounded-full transition-transform duration-300  group-hover:rotate-55'>
-          <img className='w-[0.6rem] h-auto' src="/images_assests/up-arrow.png" alt="arrow" />
-        </section>
-      </div>
+          {/* poetic lines */}
+          <div className="w-full pl-4 sm:pl-28 mt-15 sm:mt-30  sm:mb-20">
+            <h6 className='sm:text-[3vh] text-[1.2rem] funnel-regular font-bold text-[#c0c1c0]'>Every thought hums with a frequency.</h6>
+            <h6 className='sm:text-[3vh] text-[1.2rem] funnel-regular font-bold text-black'>You’re not alone in yours.</h6>
+            <h6 className='sm:text-[3vh] text-[1.2rem] funnel-regular font-bold text-[#c0c1c0]'>Vynk helps you find the ones that match.</h6>
+          </div>
 
+          {/* video */}
+          <div className="relative h-[80vh] w-full overflow-hidden z-10">
+          <video
+              className="absolute inset-0 top-1/2 transform -translate-y-1/2 left-1/2  -translate-x-1/2 h-full"
+              src="video_assests/mainvid2.mp4"
+              autoPlay
+              muted
+              loop
+              playsInline
+            ></video>
+            <span className='absolute funnel-regular text-[#565353b8] stroke-2  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[3.6rem] '>V-YNK</span>
+          </div>
+
+          {/* poetic line */}
+          <div className="w-full sm:pr-28 sm:mt-30 pr-4 mb-20 flex items-end flex-col">
+            <section className='flex sm:items-start items-end flex-col'>
+            <h6 className='text-[1.2rem] sm:text-[3.4vh]  funnel-regular font-bold text-[#c0c1c0]'>You think.</h6>
+            <h6 className='text-[1.2rem] sm:text-[3.4vh] funnel-regular font-bold text-black'>Someone else feels it too.</h6>
+            <h6 className='text-[1.2rem] sm:text-[3.4vh] funnel-regular font-bold text-[#c0c1c0]'>Vynk makes the connection.</h6>
+            </section>
+          </div>
+
+          {/* ring video */}
+          <div className="relative w-full h-[125vh] sm:h-[100vh]  sm:overflow-hidden ">
+          <video
+              className="w-full h-full scale-400 sm:scale-110"
+              src="video_assests/ringmp4.mp4"
+              autoPlay
+              muted
+              loop
+              playsInline
+            >
+            </video>
+            <div className="absolute top-1/2 left-1/2 flex justify-center items-center w-[14rem] transform -translate-x-1/2 -translate-y-1/2 loading-lazy ">
+            <h6 className="funnel-regular text-black w-full text-[1rem] sm:text-[1.1vw] leading-relaxed">
+                Vynk is a space for thoughts.<br />
+                Not every thought needs a like.<br />
+                Some just need to land.<br />
+                Somewhere soft.<br />
+                Somewhere seen.<br />
+                Vynk doesn’t care who you are —<br />
+                only what’s on your mind.<br />
+                And who else is thinking it too.
+              </h6>
+            </div>
+          </div>
+
+          {/* capsule */}
+          <div className="relative w-full h-[26rem]   overflow-hidden ">
+          <video
+              className="w-full h-full scale-130 sm:scale-120"
+              src="video_assests/capsule.mp4"
+              autoPlay
+              muted
+              loop
+              playsInline
+            >
+            </video>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+            <section className="relative min-h-screen flex items-center justify-center">
+              <h1 className="sm:text-5xl text-[1.8rem] funnel-regular text-transparent bg-clip-text bg-gradient-to-r mix-blend-exclusion from-black/80 to-black/70">
+                Beyond filters
+              </h1>
+            </section>
+            </div>
+          </div>
       </main>
     );
   }   
--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -1,13 +1,17 @@
 export default function Navbar() {
     return(
-        <nav className="h-full w-full flex justify-between items-center gap-2 ">
-            <span className="font1 text-[1.2rem] w-1/2 ml-4" >V- YNK</span>
-            <section className="flex gap-4 text-[1.2rem] font1 w-1/2 justify-between mr-4">
-                <section className="font1 flex gap-6 items-center">
-                    <span>career</span>
-                    <span>contacts</span>
-                </section>
-                <button className="font1 text-[0.9rem] place-self-end px-6  py-1 rounded-sm bg-white/10 backdrop-blur-md border border-white/20 shadow-md ">Drift in</button>
+        <nav className="bg-[#fefffe] w-full flex justify-between items-center px-4 py-2 ">
+            <section className=" flex items-center gap-1">
+                <div className="w-[0.9rem] h-[0.9rem] rounded-full bg-black"></div>
+                <span className="text-black text-lg funnel-semibold -tracking-wide">VYNK</span>
+            </section>
+            <section>
+            <button
+                onClick={() => window.open("https://mail.google.com/mail/?view=cm&to=vivek1314gurjar@gmail.com")}
+                className="funnel-light text-white bg-black rounded-3xl px-3 py-[0.3rem] text-[0.8rem]"
+                >
+                Contact
+                </button>
             </section>
         </nav>
     );
